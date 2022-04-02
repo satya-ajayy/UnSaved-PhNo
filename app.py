e9a8864cfb30320a15b4fd399e6ec2f906ab5063
rsa914016 <86813260+rsa914016@users.noreply.github.com>
2022-04-02 21:23:08 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+import webbrowser as wb
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['POST'])
+def home():
+    phno = request.form.get('phno')
+    msg = request.form.get('msg').title()
+    wb.open(f'https://api.whatsapp.com/send/?phone=91{phno}&text={msg}')
+    print(phno, msg)
+    return render_template('home.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')

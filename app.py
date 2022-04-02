cc8004458e5015d053df94d712fae7229826768a
rsa914016 <86813260+rsa914016@users.noreply.github.com>
2022-04-02 21:33:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    phno = request.form.get('phno')
-    msg = request.form.get('msg').title()
-    wb.open(f'https://api.whatsapp.com/send/?phone=91{phno}&text={msg}')
-    print(phno, msg)
+    if request.method == 'POST':
+        phno = request.form.get('phno')
+        msg = request.form.get('msg').title()
+        wb.open(f'https://api.whatsapp.com/send/?phone=91{phno}&text={msg}')
+        # print(phno, msg)
     return render_template('home.html')
 
 

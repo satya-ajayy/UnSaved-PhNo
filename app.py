b96b5d8e5d67b608a377f8b04b9a56bd5a4429b9
rsa914016 <86813260+rsa914016@users.noreply.github.com>
2022-04-02 21:27:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET', 'POST'])
 def home():
     phno = request.form.get('phno')
     msg = request.form.get('msg').title()

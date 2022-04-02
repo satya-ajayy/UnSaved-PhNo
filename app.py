c7f7c733eab097fee15d48e3668459d22e7f1cec
rsa914016 <86813260+rsa914016@users.noreply.github.com>
2022-04-02 23:19:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST':
+        phno = request.form.get('phno')
+        msg = request.form.get('msg').title()
+        return redirect(f'https://api.whatsapp.com/send/?phone=91{phno}&text={msg}')
     return render_template('home.html')
 
 

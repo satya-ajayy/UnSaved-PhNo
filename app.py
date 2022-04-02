a462a01cc6932efaac37b3bcfccb6a607a088f2b
rsa914016 <86813260+rsa914016@users.noreply.github.com>
2022-04-02 22:54:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,12 @@
-import webbrowser as wb
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    if request.method == 'POST':
-        phno = request.form.get('phno')
-        msg = request.form.get('msg').title()
-        wb.open(f'https://api.whatsapp.com/send/?phone=91{phno}&text={msg}')
-        print(phno, msg)
     return render_template('home.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0')
